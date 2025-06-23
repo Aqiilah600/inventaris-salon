26c55a022563cc476032f436740c6e2869bbbb6d
Siti Nurkholishah <situnurkholishah7@gmail.com>
2025-06-23 11:25:19 +0800

    implementasi fungsi tambah data pelanggan (addData) dan pilih paket (pilihPaket)

--- C030324054.cpp
+++ C030324054.cpp
@@ -20,3 +20,34 @@ const string daftarPaket[] = {
 const double hargaPaket[] = { 25000, 30000, 40000, 100000, 50000, 120000 };
 const int JUMLAH_PAKET = sizeof(daftarPaket)/sizeof(daftarPaket[0]);
 
+void pilihPaket(vector<Paket>& paketDipilih) {
+    int idx; char lagi;
+    do {
+        cout << "\nDaftar Paket:\n";
+        for (int i = 0; i < JUMLAH_PAKET; i++)
+            cout << i + 1 << ". " << daftarPaket[i] << " - Rp" << hargaPaket[i] << endl;
+        cout << "Pilih nomor paket: "; cin >> idx; idx--;
+        if (idx >= 0 && idx < JUMLAH_PAKET)
+            paketDipilih.push_back({daftarPaket[idx], hargaPaket[idx]});
+        else
+            cout << "Pilihan tidak valid.\n";
+        cout << "Tambah paket lagi? (y/n): "; cin >> lagi;
+    } while (lagi == 'y' || lagi == 'Y');
+    cin.ignore();
+}
+
+void addData(SalonNode*& head) {
+    SalonNode* baru = new SalonNode;
+    cout << "Nama pelanggan: "; getline(cin >> ws, baru->nama);
+    cout << "Alamat pelanggan: "; getline(cin, baru->alamat);
+    pilihPaket(baru->paketDipilih);
+    baru->next = nullptr;
+    if (!head) head = baru;
+    else {
+        SalonNode* temp = head;
+        while (temp->next) temp = temp->next;
+        temp->next = baru;
+    }
+    cout << "Data berhasil ditambahkan.\n";
+}
+
